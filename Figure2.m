5efa4c9212f20d8dd981ec21205d547300d5f19c
Vaidhiswaran Ramesh <vaidhiswaran.ramesh17@imperial.ac.uk>
2022-03-22 17:14:39 +0000

    Update Figure2.m

--- Figure2.m
+++ Figure2.m
@@ -35,16 +35,16 @@ a = xlabel('$\bf{A_{Total}}$','Interpreter','latex'); a.FontSize=18;
 savefig('fig2B_a.fig'); close all;
 
 System          = 'DSP_ComK_ComP';
-parameters      = [0.1,1,1,1,1,1,1,1,1,1,1,1,0.01,2.5,1];
-initial         = [0,30,0,0,0,0];
-ap              = [13];
+parameters      = [3,1,5,1,1,1,1,1,1,1,1,1,30,0.01,10];
+initial         = [0,0,0,0,0,0];
+ap              = [14];
 ip              = 2;
 
 fig3 = Main(System,parameters,initial,ap,ip);
 title('DSP: Common kinase and common phosphatase (M2)','FontSize',16);
-subtitle('Substrate biphasic response - $A_{pp}$ vs $A_{Total}$','Interpreter','latex');
+subtitle('Enzyme biphasic response - $A_{pp}$ vs $K_{Total}$','Interpreter','latex');
 a = ylabel('$\bf{A_{pp}}$','Interpreter','latex','rotation',0,'VerticalAlignment','middle','HorizontalAlignment','right'); a.FontSize=18;
-a = xlabel('$\bf{A_{Total}}$','Interpreter','latex'); a.FontSize=18;
+a = xlabel('$\bf{K_{Total}}$','Interpreter','latex'); a.FontSize=18;
 savefig('fig2B_b.fig'); close all;
 
 %% Panel C
