42fb681d7c0d712b052fe8f6319e0b2cdbe8392d
Vaidhiswaran Ramesh <vaidhiswaran.ramesh17@imperial.ac.uk>
2022-03-22 17:06:59 +0000

    Update Figure2.m

--- Figure2.m
+++ Figure2.m
@@ -5,7 +5,7 @@ clc
 close all
 
 %% Panel A
-% PPI system (Full model): Presence of biphasic response
+% PPI system (Full model): Enzyme Biphasic response
 System          = 'PPI_Full';
 parameters      = [1,1,1,20,1,1,1,1,30,0.01,1];
 initial         = [0,0,0,0];
@@ -20,9 +20,22 @@ a = xlabel('$\bf{A_{Total}}$','Interpreter','latex'); a.FontSize=18;
 savefig('fig2A.fig'); close all;
 
 %% Panel B
+% DSP CC: Enzyme biphasic response
+System          = 'DSP_ComK_ComP';
+parameters      = [0.1,1,1,1,1,1,1,1,1,1,1,1,0.01,2.5,1];
+initial         = [0,30,0,0,0,0];
+ap              = [13];
+ip              = 2;
 
+fig3 = Main(System,parameters,initial,ap,ip);
+title('DSP: Common kinase and common phosphatase (M2)','FontSize',16);
+subtitle('Substrate biphasic response - $A_{pp}$ vs $A_{Total}$','Interpreter','latex');
+a = ylabel('$\bf{A_{pp}}$','Interpreter','latex','rotation',0,'VerticalAlignment','middle','HorizontalAlignment','right'); a.FontSize=18;
+a = xlabel('$\bf{A_{Total}}$','Interpreter','latex'); a.FontSize=18;
+savefig('fig2C.fig'); close all;
 
 %% Panel C
+% 
 
 %% Panel D
 
