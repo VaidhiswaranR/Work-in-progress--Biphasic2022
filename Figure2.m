5c33deaeb751b6da52408787bd727a24a4cdcb58
VaidhiswaranR <58110677+VaidhiswaranR@users.noreply.github.com>
2022-03-22 17:12:27 +0000

    Update Figure2.m

--- Figure2.m
+++ Figure2.m
@@ -20,7 +20,20 @@ a = xlabel('$\bf{A_{Total}}$','Interpreter','latex'); a.FontSize=18;
 savefig('fig2A.fig'); close all;
 
 %% Panel B
-% DSP CC: Enzyme biphasic response
+% DSP CC: Substrate and Enzyme biphasic response
+System          = 'DSP_ComK_ComP';
+parameters      = [0.1,1,1,1,1,1,1,1,1,1,1,1,0.01,2.5,1];
+initial         = [0,30,0,0,0,0];
+ap              = [13];
+ip              = 2;
+
+fig2 = Main(System,parameters,initial,ap,ip);
+title('DSP: Common kinase and common phosphatase (M2)','FontSize',16);
+subtitle('Substrate biphasic response - $A_{pp}$ vs $A_{Total}$','Interpreter','latex');
+a = ylabel('$\bf{A_{pp}}$','Interpreter','latex','rotation',0,'VerticalAlignment','middle','HorizontalAlignment','right'); a.FontSize=18;
+a = xlabel('$\bf{A_{Total}}$','Interpreter','latex'); a.FontSize=18;
+savefig('fig2B_a.fig'); close all;
+
 System          = 'DSP_ComK_ComP';
 parameters      = [0.1,1,1,1,1,1,1,1,1,1,1,1,0.01,2.5,1];
 initial         = [0,30,0,0,0,0];
@@ -32,22 +45,19 @@ title('DSP: Common kinase and common phosphatase (M2)','FontSize',16);
 subtitle('Substrate biphasic response - $A_{pp}$ vs $A_{Total}$','Interpreter','latex');
 a = ylabel('$\bf{A_{pp}}$','Interpreter','latex','rotation',0,'VerticalAlignment','middle','HorizontalAlignment','right'); a.FontSize=18;
 a = xlabel('$\bf{A_{Total}}$','Interpreter','latex'); a.FontSize=18;
-savefig('fig2C.fig'); close all;
+savefig('fig2B_b.fig'); close all;
 
 %% Panel C
-% 
+% DSP CD: Substrate biphasic response
 
 %% Panel D
+% DSP DC: Enzyme biphasic response
 
 %% Panel E
+% Cascaded network: Substrate biphasic response
 
 %% Panel F
-
-%% Panel G
-
-%% Panel H
-
-%% Panel I
+% Coupled Covalent modification network: Substrate biphasic response
 
 clc
 toc
